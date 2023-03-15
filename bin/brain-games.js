de239f60e64d93791c810a9dac95ec4615cd16d9
Jesslity <treasonforpleasure@gmail.com>
2023-03-15 12:22:16 +0500

    const

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import readlineSync from "readline-sync";
 console.log("Welcome to the Brain Games!");
-var userName = readlineSync.question('May I have your name? ');
+const userName = readlineSync.question('May I have your name? ');
 console.log('Hi ' + userName + '!');
