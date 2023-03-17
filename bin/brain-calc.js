6ef6455d19cb7616ad217414e867608dd336716d
Jesslity <treasonforpleasure@gmail.com>
2023-03-17 12:45:26 +0500

    calc fix

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -10,13 +10,15 @@ console.log(`Hi ${userName}!`);
 console.log('What is the result of the expression?');
 let result = 0
 for (let i = 0; i < 3; i = i + 1 ) {
-    const a = _.random(1,100);
-    const b = _.random(1,100);
+    const a = _.random(1,10);
+    const b = _.random(1,10);
     const c = _.random(1,3);
     if (c === 1) {
-        const quEstion = readlineSync.question(`Question: ${a} * ${b} `);
+        console.log(`Question: ${a} * ${b} `);
         result = a * b;
-        if (quEstion === result) {
+        let stringResult = String(result);
+        const quEstion = readlineSync.question(`Your answer: `);
+        if (quEstion === stringResult) {
             if (i !== 2 ) {
                 console.log(`Correct!`);  
             } else {
@@ -24,13 +26,16 @@ for (let i = 0; i < 3; i = i + 1 ) {
                 console.log(`Congratulations, ${userName}!`); 
             }
         } else {
-            console.log(`${quEstion} is wrong answer ;(. Correct answer was ${result}`);
+            console.log(`'${quEstion}' is wrong answer ;(. Correct answer was '${stringResult}'`);
+            console.log(`Let's try again, ${userName}!`);
             break;
         }
     } else if (c === 2) {
-        const quEstion = readlineSync.question(`Question: ${a} + ${b} `);
+        console.log(`Question: ${a} + ${b} `);
         result = a + b;
-        if (quEstion === result) {
+        let stringResult = String(result);
+        const quEstion = readlineSync.question(`Your answer: `);
+        if (quEstion === stringResult) {
             if (i !== 2 ) {
                 console.log(`Correct!`);  
             } else {
@@ -38,13 +43,16 @@ for (let i = 0; i < 3; i = i + 1 ) {
                 console.log(`Congratulations, ${userName}!`); 
             }
         } else {
-            console.log(`${quEstion} is wrong answer ;(. Correct answer was ${result}`);
+            console.log(`'${quEstion}' is wrong answer ;(. Correct answer was '${stringResult}'`);
+            console.log(`Let's try again, ${userName}!`);
             break;
         }
     } else if (c === 3) {
-        const quEstion = readlineSync.question(`Question: ${a} - ${b} `);
+        console.log(`Question: ${a} - ${b} `);
         result = a - b;
-        if (quEstion === result) {
+        let stringResult = String(result);
+        const quEstion = readlineSync.question(`Your answer: `);
+        if (quEstion === stringResult) {
             if (i !== 2 ) {
                 console.log(`Correct!`);  
             } else {
@@ -52,7 +60,8 @@ for (let i = 0; i < 3; i = i + 1 ) {
                 console.log(`Congratulations, ${userName}!`); 
             }
         } else {
-            console.log(`${quEstion} is wrong answer ;(. Correct answer was ${result}`);
+            console.log(`'${quEstion}' is wrong answer ;(. Correct answer was '${stringResult}'`);
+            console.log(`Let's try again, ${userName}!`);
             break;
         };
     };
