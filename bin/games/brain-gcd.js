e52eb35e186f73b51ab4dbb8ce6e2a1980f1017a
Jesslity <treasonforpleasure@gmail.com>
2023-03-20 11:29:49 +0500

    gcd

---
+++ bin/games/brain-gcd.js
@@ -0,0 +1,9 @@
+import readlineSync from "readline-sync";
+import _ from "lodash";
+
+console.log(`brain-gcd`);
+
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name?');
+console.log(`Hi ${userName}!`);
+console.log('Find the greatest common divisor of given numbers.');
