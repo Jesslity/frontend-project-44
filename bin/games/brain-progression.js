f66ac9ac3ee2877ed8792079f2de75ce129cc323
Jesslity <treasonforpleasure@gmail.com>
2023-03-27 18:00:32 +0500

    prog rel

--- bin/games/brain-progression.js
+++ bin/games/brain-progression.js
@@ -2,18 +2,22 @@
 import readlineSync from 'readline-sync';
 import _ from 'lodash';
 
-let a = _.random(1, 10);
-let b = _.random(1, 10);
-let c = _.random(1, 10);
-const numba = [];
+
+const mumba = [];
 const brainProg = (a, b, c) => {
+    const numba = [];  
     for (let i = 0; i < 10; i = i + 1) {
         numba.push(a);
         a = a + b;
     }
-    numba.splice(c, 1, '..');
-    return numba;
+    mumba.splice(0);
+    mumba.push(numba[c]);
+    mumba.join();
+    numba.splice(c, 1, '..');    
+    numba.join(' ');
+    return numba.join(' ');
 };
+
 
 console.log('brain-progression');
 console.log('Welcome to the Brain Games!');
@@ -22,15 +26,19 @@ console.log(`Hi ${userName}!`);
 console.log('What number is missing in the progression?');
 
 for (let i = 0; i < 3; i += 1) {
+    let a = _.random(1, 10);
+    let b = _.random(1, 10);
+    let c = _.random(1, 10);
     console.log(`Question ${brainProg(a,b,c)}`);
     const quEstion = readlineSync.question('Your answer: ');
-    if (z = quEstion && i !== 2) {
+    if (mumba.join() == quEstion && i !== 2) {
         console.log('Correct!');
-    } else if (z = quEstion) {
+    } else if (mumba.join() == quEstion) {
         console.log('Correct!');
         console.log(`Congratulations, ${userName}!`);
     } else {
-        console.log(`${quEstion} is wrong answer ;(. Correct answer was ${z}`);
-        console.log(`Let's try again, ${userName}!`); 
+        console.log(`'${quEstion}' is wrong answer ;(. Correct answer was '${mumba.join()}'`);
+        console.log(`Let's try again, ${userName}!`);
+        break; 
     }
 };
