327e26e0e841cefbb8a82ab863ca63a4f3cf2b53
Jesslity <treasonforpleasure@gmail.com>
2023-04-06 12:11:07 +0500

    gcd-fix

---
+++ src/games/brain-gcd.js
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+import startGame from '../index.js';
+
+const rules = 'Find the greatest common divisor of given numbers.';
+const gcd = (number1, number2) => {
+  if (number1 > number2) {
+    while (number1 % number2 !== 0) {
+      const number3 = number1 % number2;
+      number1 = number2;
+      number2 = number3;
+    }
+    return number2;
+  }
+  while (number2 % number1 !== 0) {
+    const number3 = number2 % number1;
+    number2 = number1;
+    number1 = number3;
+  }
+  return number1;
+};
+
+const game = () => {
+  const randomNumber1 = _.random(10, 100);
+  const randomNumber2 = _.random(10, 100);
+  const question = `${randomNumber1} ${randomNumber2}`;
+  const result = gcd(randomNumber1, randomNumber2);
+  return [String(result), question];
+};
+
+export default () => startGame(rules, game);
