2adf92c793ad026ea81c9f12aed0ca6aab1e3ef4
Jesslity <treasonforpleasure@gmail.com>
2023-04-06 12:34:16 +0500

    progg-fix1

---
+++ src/games/brain-progression.js
@@ -0,0 +1,29 @@
+import _ from 'lodash';
+import startGame from '../index.js';
+
+const rules = 'What number is missing in the progression?';
+const arrResult = [];
+const brainProgression = (firstNumber, step, missingNumber) => {
+  const arrStep = [];
+  for (let i = 0; i < 10; i += 1) {
+    arrStep.push(firstNumber);
+    firstNumber += step;
+  }
+  arrResult.splice(0);
+  arrResult.push(arrStep[missingNumber]);
+  arrResult.join();
+  arrStep.splice(missingNumber, 1, '..');
+  arrStep.join(' ');
+  return arrStep.join(' ');
+};
+
+const game = () => {
+  const firstNumber = _.random(1, 10);
+  const step = _.random(1, 10);
+  const missingNumber = _.random(1, 10);
+  const question = `${brainProgression(firstNumber, step, missingNumber)}`;
+  const result = arrResult.join();
+  return [result, question];
+};
+
+export default () => startGame(rules, game);
